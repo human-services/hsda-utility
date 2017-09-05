2469d878138df3ae572eba6f67d9ffa4792c71e2
Kin Lane <info@apievangelist.com>
2017-09-04 20:10:02 -0400

    update meta

--- includes/post.php
+++ includes/post.php
@@ -7,9 +7,9 @@ $request = $app->request();
 $_get = $request->params();
 $_get = filter_var_array($_get,FILTER_SANITIZE_STRING);
 $_body = $request->getBody();
-$_body = json_decode($_body,true);	
+$_body = json_decode($_body,true);
 $_body = filter_var_array($_body,FILTER_SANITIZE_STRING);
-		
+
 // Override any ID
 $local_id = getGUID();
 $_body['id'] = $local_id;
@@ -30,29 +30,29 @@ $response_200 = $responses['200'];
 // grab our schema
 $schema_ref = $response_200['schema']['items']['$ref'];
 $schema = str_replace("#/definitions/","",$schema_ref);
-$schema_properties = $definitions[$schema]['properties'];	
+$schema_properties = $definitions[$schema]['properties'];
 $schema = str_replace("_complete","",$schema);
 
-$path_count_array = explode("/",$route);	
-$path_count = count($path_count_array);	
+$path_count_array = explode("/",$route);
+$path_count = count($path_count_array);
 $core_path = $path_count_array[1];
 $core_path = substr($core_path,0,strlen($core_path)-1);
 //echo "path: " . $core_path . "<br />";
 if(isset($_body[$core_path . "_id"])){ $_body[$core_path . "_id"] = $id; }
 
 // Load any pre extensions for this route
-if (file_exists($prepath)) 
+if (file_exists($prepath))
 	{
 	include $prepath;
 	}
 
 // Meta
-include "meta/pre.php";	
+include "meta/pre.php";
 
 // override primary query
 if($override==0)
 	{
-	
+
 	// Fields
 	$field_string = "";
 	foreach($schema_properties as $field => $value)
@@ -60,44 +60,51 @@ if($override==0)
 		if(isset($value['type']) && $value['type'] != 'array')
 			{
 			if(isset($_body[$field]))
-				{			
+				{
 				$field_string .= $field . ",";
 				}
 			}
 		else
 			{
-			// Deal With Array	
+			// Deal With Array
 			}
 		}
 	if(substr($field_string,strlen($field_string)-1,1) == ","){ $field_string = substr($field_string,0,strlen($field_string)-1); }
-	
+
 	// Values
 	$value_string = "";
 	foreach($schema_properties as $field => $value)
 		{
 		if(isset($value['type']) && $value['type'] != 'array')
-			{			
+			{
 			if(isset($_body[$field]))
-				{			
-				$value_string .= "'" . $_body[$field] . "',";
+				{
+				if(is_array( $_body[$field]))
+					{
+					$value_string .= "'" . stripslashes(format_json(json_encode($_body[$field]))) . "',";
+					}
+				else
+					{
+					$value_string .= "'" . $_body[$field] . "',";
+					}
 				}
 			}
 		else
 			{
-			// Deal With Array	
-			}	
-		}	
+			// Deal With Array
+			}
+		}
 	if(substr($value_string,strlen($value_string)-1,1) == ","){ $value_string = substr($value_string,0,strlen($value_string)-1); }
-	
+
 	// Build The Query To Insert
 	$query = "INSERT INTO " . $schema ."(" . $field_string . ") VALUES(" . $value_string . ")";
 	//echo "\n" . $query . "\n";
-	
+
 	// Execute Query
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$response = $conn->exec($query);
 	//echo $response . "\n";
-	
+
 	// We need to do something if fails -- return proper status code
 	if($response==1)
 		{
@@ -112,34 +119,34 @@ if($override==0)
 	foreach($schema_properties as $field => $value)
 		{
 		if(isset($value['type']) && $value['type'] != 'array')
-			{			
+			{
 			$F[$field] = filter_var($_body[$field], FILTER_SANITIZE_STRING);
 			}
 		else
 			{
-			// Deal With Array	
-			
-			$path_count_array = explode("/",$route);	
-			$path_count = count($path_count_array);	
+			// Deal With Array
+
+			$path_count_array = explode("/",$route);
+			$path_count = count($path_count_array);
 			$core_path = $path_count_array[1];
 			$core_path = substr($core_path,0,strlen($core_path)-1);
 			//echo "path: " . $core_path . "<br />";
-			//echo "path count: " . $path_count . "<br />";				
-						
+			//echo "path count: " . $path_count . "<br />";
+
 			$sub_schema_ref = $value['items']['$ref'];
 			$sub_schema = str_replace("#/definitions/","",$sub_schema_ref);
 			$sub_schema_properties = $definitions[$sub_schema]['properties'];
 			//echo $sub_schema . "\n";
-			//var_dump($sub_schema_properties);	
-	
+			//var_dump($sub_schema_properties);
+
 			foreach($_body[$field] as $sub_body)
 				{
-	
+
 				// Override any ID
 				$sub_id = getGUID();
-				$sub_body['id'] = $sub_id;	
-				$sub_body[$core_path . '_id'] = $local_id;	
-				
+				$sub_body['id'] = $sub_id;
+				$sub_body[$core_path . '_id'] = $local_id;
+
 				// Fields
 				$field_string = "";
 				foreach($sub_schema_properties as $sub_field_1 => $sub_value_1)
@@ -147,73 +154,73 @@ if($override==0)
 					if(isset($sub_value_1['type']))
 						{
 						if(isset($sub_body[$sub_field_1]))
-							{			
+							{
 							$field_string .= $sub_field_1 . ",";
 							}
 						}
 					else
 						{
-						// Deal With Array	
+						// Deal With Array
 						}
 					}
 				if(substr($field_string,strlen($field_string)-1,1) == ","){ $field_string = substr($field_string,0,strlen($field_string)-1); }
-				
+
 				// Values
 				$value_string = "";
 				foreach($sub_schema_properties as $sub_field_2 => $sub_value_2)
 					{
 					if(isset($sub_value_2['type']))
-						{			
+						{
 						if(isset($sub_body[$sub_field_2]))
-							{			
+							{
 							$value_string .= "'" . $sub_body[$sub_field_2] . "',";
 							}
 						}
 					else
 						{
-						// Deal With Array	
-						}	
-					}	
+						// Deal With Array
+						}
+					}
 				if(substr($value_string,strlen($value_string)-1,1) == ","){ $value_string = substr($value_string,0,strlen($value_string)-1); }
-			
+
 				// Build The Query To Insert
 				$query = "INSERT INTO " . $sub_schema ."(" . $field_string . ") VALUES(" . $value_string . ")";
 				//echo "\n" . $query . "\n";
-				
+
 				// Execute Query
 			    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			    $response = $conn->exec($query);
-				//echo $response . "\n";				
-				
+				//echo $response . "\n";
+
 				}
-			
+
 			$F[$field] = $_body[$field];
-			
-			}				
-		}	
+
+			}
+		}
 	}
-	
+
 $ReturnObject = $F;
-	
-// Load any post extensions for this path	
-if (file_exists($postpath)) 
+
+// Load any post extensions for this path
+if (file_exists($postpath))
 	{
 	include $postpath;
-	}	
-	
+	}
+
 // Meta
-include "meta/post.php";	
+include "meta/post.php";
 
 //echo $head['ACCEPT'] . "<br />";
 if(isset($head['ACCEPT']) && $head['ACCEPT'] == 'text/csv')
 	{
-	$app->response()->header("Content-Type", "text/csv");	
+	$app->response()->header("Content-Type", "text/csv");
 	$return_csv = generateCsv($ReturnObject);
 	echo $return_csv;
 	}
 elseif(isset($head['ACCEPT']) && $head['ACCEPT'] == 'application/xml')
 	{
-	$app->response()->header("Content-Type", "application/xml");	
+	$app->response()->header("Content-Type", "application/xml");
 	$return_xml = arrayToXml($ReturnObject);
 	echo $return_xml;
 	}
